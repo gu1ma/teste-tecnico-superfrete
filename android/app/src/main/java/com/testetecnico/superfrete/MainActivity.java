520cc3dc0a62a6b4f8e64700b9fdb434a4112cf2
Gabriel Silva <gabrielgs1408@gmail.com>
2024-08-11 12:21:45 -0300

    feat: installing capacitor for android

---
+++ android/app/src/main/java/com/testetecnico/superfrete/MainActivity.java
@@ -0,0 +1,5 @@
+package com.testetecnico.superfrete;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.testetecnico.superfrete',
+  appName: 'teste-tecnico-superfrete',
+  webDir: 'build'
+};
+
+export default config;
