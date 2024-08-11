0d0bad9db670396125dfa3512a27b075acc800ca
Gabriel Silva <gabrielgs1408@gmail.com>
2024-08-11 19:44:26 -0300

    feat: adding loader

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Text } from "@chakra-ui/react";
+import { Box, Center, Spinner, Stack, Text } from "@chakra-ui/react";
 import Message from "./Message";
 import { useEffect, useState } from "react";
 import { collection, DocumentData, onSnapshot, QuerySnapshot } from "firebase/firestore";
@@ -17,6 +17,7 @@ export type MessageType = {
 
 export default function MessageList() {
     const [messages, setMessages] = useState<MessageType[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const formatDate = (dateInMillis: number) => {
         const date = DateTime.fromMillis(Math.trunc(Number(dateInMillis)));
@@ -25,7 +26,6 @@ export default function MessageList() {
 
     useEffect(() => {
         const colRef = collection(db, "messages");
-
         const unsubscribe = onSnapshot(colRef, (snapshot: QuerySnapshot<DocumentData>) => {
             const updatedData: MessageType[] = snapshot.docs.map(doc => ({
                 id: doc.id,
@@ -33,6 +33,7 @@ export default function MessageList() {
             }));
             const sortedData = updatedData.sort((a, b) => b.date.seconds - a.date.seconds);
             setMessages(sortedData);
+            setLoading(false); 
         });
 
         return () => unsubscribe();
@@ -43,17 +44,30 @@ export default function MessageList() {
             <Text fontWeight="bold" mb={2}>
                 Mensagens enviadas
             </Text>
-            <Stack spacing={2} h="55vh" overflow="scroll">
-                {
-                    messages.map((message) => (
-                        <Message
-                            key={message.date.seconds}
-                            message={message.content}
-                            date={formatDate(message.date.seconds * 1000)}
+            {
+                loading ? (
+                    <Center>
+                        <Spinner
+                            thickness="4px"
+                            speed="0.65s"
+                            emptyColor="gray.200"
+                            color="green.500"
+                            size="xl"
                         />
-                    ))
-                }
-            </Stack>
+                    </Center>
+                ) : 
+                    <Stack spacing={2} h="55vh" overflow="scroll">
+                        {
+                            messages.map((message) => (
+                                <Message
+                                    key={message.date.seconds}
+                                    message={message.content}
+                                    date={formatDate(message.date.seconds * 1000)}
+                                />
+                            ))
+                        }
+                </Stack>
+            }
         </Box>
     )
 }
